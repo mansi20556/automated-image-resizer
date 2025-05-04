ec915068ff04dd939ece0134cbe30724558fc197
Mansi Jain <jainmansi668@gmail.com>
2025-05-05 00:20:21 +0530

    adding lambda layer for pillow

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,10 @@ resource "aws_lambda_function" "resizer" {
       DEST_BUCKET = aws_s3_bucket.dest_bucket.bucket
     }
   }
+
+  layers = [
+    "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-Pillow:12"
+  ]
 }
 
 # ✅ Permission to allow S3 to invoke Lambda
