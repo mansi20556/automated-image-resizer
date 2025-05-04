60492245664745e5922db0bb1f3d4bbec7e08c4c
Mansi Jain <jainmansi668@gmail.com>
2025-05-04 22:35:46 +0530

    adding backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate-backend-resize"
+    key    = "terraform/state.tfstate"  # You can specify a path within the bucket
+    region = "us-east-1"  # Set your region
+  }
+}
